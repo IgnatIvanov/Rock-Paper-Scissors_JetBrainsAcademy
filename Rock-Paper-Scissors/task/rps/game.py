cdfaf5d8b975c67528c4c29042065bee2cbb9cba
IgnatIvanov <ignativanov1996@mail.ru>
2020-09-28 20:55:47 +0300

    2020.09.28.2055 Stage 2 complete. Some problems solved.

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,9 +1,25 @@
 # Write your code here
-user_move = input()
+import random
 
-if user_move == "paper":
-    print("Sorry, but the computer chose scissors")
+options_dict = {"1": "paper", "2": "scissors", "3": "rock"}
+
+user_move = input()
+computer_move = options_dict[str(random.randint(1, 3))]
+
+if user_move == computer_move:
+    print("There is a draw (" + user_move + ")")
+elif user_move == "paper":
+    if computer_move == "rock":
+        print("Well done. The computer chose " + computer_move + " and failed")
+    else:
+        print("Sorry, but the computer chose " + computer_move)
 elif user_move == "scissors":
-    print("Sorry, but the computer chose rock")
+    if computer_move == "paper":
+        print("Well done. The computer chose " + computer_move + " and failed")
+    else:
+        print("Sorry, but the computer chose " + computer_move)
 elif user_move == "rock":
-    print("Sorry, but the computer chose paper")
+    if computer_move == "scissors":
+        print("Well done. The computer chose " + computer_move + " and failed")
+    else:
+        print("Sorry, but the computer chose " + computer_move)
