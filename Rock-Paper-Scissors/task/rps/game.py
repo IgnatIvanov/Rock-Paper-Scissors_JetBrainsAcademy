dedd779a2f867b4242f025929fcc52eee065dd1e
IgnatIvanov <ignativanov1996@mail.ru>
2020-09-28 20:27:11 +0300

    2020.09.28.2026 Initial commit. Stage 1 complete.

---
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -0,0 +1,9 @@
+# Write your code here
+user_move = input()
+
+if user_move == "paper":
+    print("Sorry, but the computer chose scissors")
+elif user_move == "scissors":
+    print("Sorry, but the computer chose rock")
+elif user_move == "rock":
+    print("Sorry, but the computer chose paper")
