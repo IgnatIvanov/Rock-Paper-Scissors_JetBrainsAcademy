e0921a52339fa147b13d76e1d164abcf640727a0
IgnatIvanov <ignativanov1996@mail.ru>
2020-10-04 18:57:23 +0300

    2020.10.04.1856 Project complete. Some problems complete.

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,8 +1,13 @@
 # Write your code here
 import random
 
-options_dict = {"1": "paper", "2": "scissors", "3": "rock"}
-#rating = dict()
+options_dict = list(['scissors', 'paper', 'rock'])
+all_options = list(['rock', 'fire', 'scissors', 'snake', 'human',
+                    'tree', 'wolf', 'sponge', 'paper', 'air',
+                    'water', 'dragon', 'devil', 'lightning', 'gun',
+                    'rock', 'fire', 'scissors', 'snake', 'human',
+                    'tree', 'wolf', 'sponge', 'paper', 'air',
+                    'water', 'dragon', 'devil', 'lightning', 'gun'])
 current_rating = 0
 
 print('Enter your name:', end=" ")
@@ -15,13 +20,16 @@ for line in ratings_file:
     if args[0] == user_name:
         current_rating = int(args[1])
 
+dict_input = input()
+if dict_input != '':
+    options_dict = dict_input.split(',')
+print("Okay, let's start")
+
 while True:
     user_move = input()
-    computer_move = options_dict[str(random.randint(1, 3))]
+    computer_move = options_dict[random.randint(0, len(options_dict)-1)]
 
-    if user_move not in ("rock", "scissors", "paper", "!exit", "!rating"):
-        print("Invalid input")
-    elif user_move == "!exit":
+    if user_move == "!exit":
         print("Bye!")
         break
     elif user_move == "!rating":
@@ -29,21 +37,12 @@ while True:
     elif user_move == computer_move:
         print("There is a draw (" + user_move + ")")
         current_rating += 50
-    elif user_move == "paper":
-        if computer_move == "rock":
+    elif user_move in options_dict:
+        user_index = all_options.index(user_move)
+        if computer_move in all_options[user_index:user_index+8]:
             print("Well done. The computer chose " + computer_move + " and failed")
             current_rating += 100
         else:
             print("Sorry, but the computer chose " + computer_move)
-    elif user_move == "scissors":
-        if computer_move == "paper":
-            print("Well done. The computer chose " + computer_move + " and failed")
-            current_rating += 100
-        else:
-            print("Sorry, but the computer chose " + computer_move)
-    elif user_move == "rock":
-        if computer_move == "scissors":
-            print("Well done. The computer chose " + computer_move + " and failed")
-            current_rating += 100
-        else:
-            print("Sorry, but the computer chose " + computer_move)
+    else:
+        print("Invalid input")
